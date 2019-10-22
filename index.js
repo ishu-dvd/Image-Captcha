7edd8db908a9ed729a3e53d3f09b898ef57ba7b4
ishu-dvd <imishu98@gmail.com>
2019-10-22 08:00:23 +0530

    added bs3 and compiled some parts

---
+++ index.js
@@ -0,0 +1,9 @@
+function startCaptcha(){
+  var captchaList = ["colorSpree.html" , "colorDilemma.html" , "dragIt.html"];
+  var choice = Math.floor(((Math.random())*1000)%3) ;
+  console.log(captchaList[choice]);
+  choice = 0 ; //Change the hard coded with random values .....
+
+  var firstCaptcha = captchaList[choice] ;
+  window.location.replace(firstCaptcha) ;
+}
